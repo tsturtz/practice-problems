5dca3a79d5be6198228dff50cb0952a04eece175
Taylor Sturtz <taylorsturtz.com>
2016-10-31 09:53:44 -0700

    buzz .. fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,11 +4,22 @@
 
 
 
-function () {
-    for (var i = 1; i <= 100; i++) {
-        if (i%3 ==) {
-
+function fizzbuzz() {
+    for (var i = 1; i < 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0)  {
+            console.log('FizzBuzz');
         }
-        console.log(i);
+        else if (i % 3 === 0) {
+            console.log('Fizz');
+        }
+        else if (i % 5 === 0) {
+            console.log('Buzz');
+        }
+        else {
+            console.log(i);
+        }
     }
+    return i;
 }
+
+console.log(fizzbuzz());
